7422fb6751f782f98afb62d337b80465f2792aa4
zeriod <106491926+zeriod@users.noreply.github.com>
2022-05-29 23:07:26 +0200

    Update zeriodtool.py

--- zeriodtool.py
+++ zeriodtool.py
@@ -19,6 +19,6 @@ def iplocator():
 	print("***************************Bienvenue sur Tracer Une IP********************************")
 	print("**************************************************************************************")
 	ip = input("\n\nMettez l'adress IP que vous souhaitez localisé : ")
-	os.system(curl -s ipinfo.io/"$1" | sed 's/"//g' | awk 'NR>2 {print last} {last=$0}')
+	system(curl -s ipinfo.io/"$1" | sed 's/"//g' | awk 'NR>2 {print last} {last=$0}')
 if __name__ == "__main__":
 	main()
